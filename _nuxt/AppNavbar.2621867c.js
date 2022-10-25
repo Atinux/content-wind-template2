3a89abd2a344f8578e542baf60dde47d53e26ee5
Atinux <seb@nuxtlabs.com>
2022-10-25 20:35:45 +0000

    Deploying to gh-pages from @ Atinux/content-wind-template2@07b4644aff84ef9a0aa59640c11eca493c9a580a 🚀

---
+++ _nuxt/AppNavbar.2621867c.js
@@ -0,0 +1,1 @@
+import"./entry.9e40912e.js";import{Q as r}from"./entry.9e40912e.js";export{r as default};
---
+++ _nuxt/ProseStrong.822ba78a.js
@@ -0,0 +1,1 @@
+import{a as o,o as r,b as t,m as n}from"./entry.9e40912e.js";const s={};function a(e,c){return r(),t("strong",null,[n(e.$slots,"default")])}const f=o(s,[["render",a]]);export{f as default};
